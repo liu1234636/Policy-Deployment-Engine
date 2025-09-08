4d872a8a307443fddf351b217c93278186df22d0
rui <s223943884@deakin.edu.au>
2025-09-08 13:14:50 +0800

    Revert "git main change"

--- policies/policy/useless/nc.tf
+++
@@ -1,8 +0,0 @@
-resource "google_firestore_database" "wrong" {
-  name                          = "wrong-db"
-  location_id                   = "australia-southeast1"
-  type                          = "FIRESTORE_NATIVE"
-  # lack point_in_time_recovery_enable → default false
-  # lack app_engine_integration    → default true()
-  # lack concurrency_mode          → default empty
-}
